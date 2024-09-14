c6db953cdf17af9f8e17fad147611da66f45d3a5
Potato-Man5 <potatoman5520@gmail.com>
2024-09-14 12:47:08 +0800

    second commit

--- Martin_HOA1External.js
+++ Martin_HOA1External.js
@@ -3,4 +3,11 @@ var y = 6;
 var z = x + y;
 function myFunction() {
 document.getElementById("demo").innerHTML="The value of z is:" + z ;
+// document.getElementById("animate-image").classList.add("move");
+var img = document.getElementById("animate-image");
+      img.style.visibility = "visible";
+      img.classList.add("move");
+      setTimeout(function() {
+        img.style.visibility = "hidden";
+      }, 2000);
 }
